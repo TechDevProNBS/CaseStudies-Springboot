28d23bc0273f70f70235145b03f60243e958dd89
Ryan Kimpton <RyanKimpton965@gmail.com>
2019-12-03 11:55:11 +0000

    Added add, update, delete, show all, and filter functionality

--- src/main/java/com/nationwide/groupproject/Controllers/DataController.java
+++ src/main/java/com/nationwide/groupproject/Controllers/DataController.java
@@ -1,8 +1,11 @@
 package com.nationwide.groupproject.Controllers;
 
+import com.nationwide.groupproject.Entities.Data;
 import com.nationwide.groupproject.Repositories.DataRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @CrossOrigin("*")
@@ -12,6 +15,59 @@ public class DataController {
     @Autowired
     public DataController(DataRepo repo){
         this.repo = repo;
+    }
+
+    @PostMapping("/AddRecord")
+    public Data addRecord(@RequestBody Data data){
+        repo.saveAndFlush(data);
+        return data;
+    }
+
+    @PostMapping("/UpdateRecord")
+    public Data updateRecord(@RequestBody Data data){
+        repo.save(data);
+        return data;
+    }
+
+    @DeleteMapping("/DeleteRecord/{I}")
+    public void deleteRecord(@PathVariable long I){
+        repo.delete(repo.findById(I));
+    }
+
+    @GetMapping("/ShowRecords")
+    public ArrayList<Data> showAllRecords(){
+        return repo.findAll();
+    }
+
+    @GetMapping("/Filter/{Stream}/{Year}/{Role}/{Location}")
+    public ArrayList<Data> filterRecords(@PathVariable String stream, @PathVariable String year, @PathVariable String role, @PathVariable String location){
+        String query = "SELECT * FROM tdp_table WHERE ";
+
+        if(stream.equals("-1")){
+            query += "NOT stream IS NULL AND";
+        } else {
+            query += "stream=" + stream;
+        }
+
+        if(year.equals("-1")){
+            query += "NOT year IS NULL AND";
+        } else {
+            query += "year=" + year;
+        }
+
+        if(role.equals("-1")){
+            query += "NOT role IS NULL AND";
+        } else {
+            query += "role=" + role;
+        }
+
+        if(location.equals("-1")){
+            query += "NOT location IS NULL";
+        } else {
+            query += "location=" + location;
+        }
+
+        return repo.filter(query);
     }
 
 }
