2712676640ae3e75b861758b4b68d805f0d741b7
Ryan Kimpton <RyanKimpton965@gmail.com>
2019-12-08 17:59:08 +0000

    Changed some column names to bring in line with what Mark would like

--- src/main/java/com/nationwide/entities/Data.java
+++ src/main/java/com/nationwide/entities/Data.java
@@ -16,22 +16,15 @@ public class Data {
     private String facebook;
     private String linkedin;
     private String twitter;
-    //private String phoneNumber;
-    //private String area;
-     private String startdate;
-    //private String programme;
-    //private String internalExternal;
-    //private String jobTitle;
-    //private String locationDescription;
-    //private String locationAddress;
-    //private String districtCode;
-    //This corresponds to district description:
-    private String currentrole;
-    //private String areaCode;
-    //private String departmentDescription;
+    private String area;
+    private String startdate;
+    private String programme;
+    private String internalexternal;
+    private String locationdescription;
+    private String districtdescription;
     private String photo;
 
-    public Data(String name, String stream, String background, String maintext, String email, String facebook, String linkedin, String twitter, String startdate, String currentrole, String photo){
+    public Data(String name, String stream, String background, String maintext, String email, String facebook, String linkedin, String twitter, String area, String startdate, String programme, String internalexternal, String locationdescription, String currentrole, String districtdescription, String photo){
         this.name = name;
         this.stream = stream;
         this.background = background;
@@ -40,18 +33,12 @@ public class Data {
         this.facebook = facebook;
         this.linkedin = linkedin;
         this.twitter = twitter;
-        //this.phoneNumber = phoneNumber;
-        //this.area = area;
+        this.area = area;
         this.startdate = startdate;
-//        this.programme = programme;
-//        this.internalExternal = internalExternal;
-//        this.jobTitle = jobTitle;
-//        this.locationDescription = locationDescription;
-//        this.locationAddress = locationAddress;
-//        this.districtCode = districtCode;
-        this.currentrole = currentrole;
-//        this.areaCode = areaCode;
-//        this.departmentDescription = departmentDescription;
+        this.programme = programme;
+        this.internalexternal = internalexternal;
+        this.locationdescription = locationdescription;
+        this.districtdescription = districtdescription;
         this.photo = photo;
     }
 
@@ -121,21 +108,46 @@ public class Data {
         this.twitter = twitter;
     }
 
-//    public String getPhoneNumber() {
-//        return phoneNumber;
-//    }
-//
-//    public void setPhoneNumber(String phoneNumber) {
-//        this.phoneNumber = phoneNumber;
-//    }
-//
-//    public String getArea() {
-//        return area;
-//    }
-//
-//    public void setArea(String area) {
-//        this.area = area;
-//    }
+
+    public String getStartdate() {
+        return startdate;
+    }
+
+    public void setStartdate(String startdate) {
+        this.startdate = startdate;
+    }
+
+    public String getProgramme() {
+        return programme;
+    }
+
+    public void setProgramme(String programme) {
+        this.programme = programme;
+    }
+
+    public String getInternalexternal() {
+        return internalexternal;
+    }
+
+    public void setInternalexternal(String internalexternal) {
+        this.internalexternal = internalexternal;
+    }
+
+    public String getLocationdescription() {
+        return locationdescription;
+    }
+
+    public void setLocationdescription(String locationdescription) {
+        this.locationdescription = locationdescription;
+    }
+
+    public String getDistrictdescription() {
+        return districtdescription;
+    }
+
+    public void setDistrictdescription(String districtdescription) {
+        this.districtdescription = districtdescription;
+    }
 
     public String getJoinDate() {
         return startdate;
@@ -145,78 +157,13 @@ public class Data {
         this.startdate = joinDate;
     }
 
-//    public String getProgramme() {
-//        return programme;
-//    }
-//
-//    public void setProgramme(String programme) {
-//        this.programme = programme;
-//    }
-//
-//    public String getInternalExternal() {
-//        return internalExternal;
-//    }
-//
-//    public void setInternalExternal(String internalExternal) {
-//        this.internalExternal = internalExternal;
-//    }
-//
-//    public String getJobTitle() {
-//        return jobTitle;
-//    }
-//
-//    public void setJobTitle(String jobTitle) {
-//        this.jobTitle = jobTitle;
-//    }
-//
-//    public String getLocationDescription() {
-//        return locationDescription;
-//    }
-//
-//    public void setLocationDescription(String locationDescription) {
-//        this.locationDescription = locationDescription;
-//    }
-//
-//    public String getLocationAddress() {
-//        return locationAddress;
-//    }
-//
-//    public void setLocationAddress(String locationAddress) {
-//        this.locationAddress = locationAddress;
-//    }
-//
-//    public String getDistrictCode() {
-//        return districtCode;
-//    }
-//
-//    public void setDistrictCode(String districtCode) {
-//        this.districtCode = districtCode;
-//    }
-
-    public String getCurrentrole() {
-        return currentrole;
+    public String getArea() {
+        return area;
     }
 
-    public void setCurrentrole(String currentrole) {
-        this.currentrole = currentrole;
+    public void setArea(String area) {
+        this.area = area;
     }
-
-//    public String getAreaCode() {
-//        return areaCode;
-//    }
-//
-//    public void setAreaCode(String areaCode) {
-//        this.areaCode = areaCode;
-//    }
-//
-//    public String getDepartmentDescription() {
-//        return departmentDescription;
-//    }
-//
-//    public void setDepartmentDescription(String departmentDescription) {
-//        this.departmentDescription = departmentDescription;
-//    }
-
 
     public String getPhoto() {
         return photo;
